f8d00d42c83457e1284bf53e35ee0b2951bcfc92
JeffreyMeijer <123974@student.drenthecollege.nl>
2020-04-03 11:21:34 +0200

    Oefenopdracht 3

---
+++ Oefenopdrachten/src/opdrachten1tot3/Runner.java
@@ -0,0 +1,15 @@
+package opdrachten1tot3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Runner {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(200,200);
+        frame.setLayout(new FlowLayout());
+        frame.add(new Controller());
+    }
+}
